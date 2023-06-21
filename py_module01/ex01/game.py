7aa0d93b670ec93f7f4a66ae2bc7fb9cb43ed1b8
elhayyany <elhayyanyabdelaziz@gmail.com>
2023-06-21 15:48:42 +0100

    finishing ex01

---
+++ py_module01/ex01/game.py
@@ -0,0 +1,16 @@
+class GotCharakter:
+    def __init__(self, first_name, is_alive = True):
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+class Stark(GotCharakter):
+    def __init__(self, first_name=None, is_alive = True ):
+        super().__init__(first_name, is_alive)
+        self.family_name = "Stark"
+        self.house_words = "winter is comming"
+
+    def print_house_words(self):
+        print(self.house_words)
+
+    def die(self):
+        self.is_alive = False
