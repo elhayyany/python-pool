1655400cc015906589c6c8e29580e74ae31d4f9a
Abdelaziz Elhayyany <elhayyanyabdelaziz@gmail.com>
2023-06-21 11:26:47 +0100

    creat game.py

--- py_module01/ex00/test.py
+++ py_module01/ex00/test.py
@@ -1,3 +1,11 @@
-from book import Book
-from recipe import Recipe
+# from book import Book
+# from recipe import Recipe
 
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
