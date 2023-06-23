6fa5ad7a878ebf743716867a059aa588971eb90e
elhayyany <elhayyanyabdelaziz@gmail.com>
2023-06-23 21:20:54 +0100

    /ex01

--- py_module01/ex02/vector.py
+++ py_module01/ex02/vector.py
@@ -1,10 +1,14 @@
 class Vector:
-    def __init__(self, values):
-        self.values = values
+    def calculate_shape(values):
         if not isinstance(values, list) or not all(len(values) != 1 and len(values[x]) != 1 for x in values):
             raise ValueError("bad atgument")
         if len(values) == 1:
-            self.shape = (1, len(values[0]))
-        else:
-            self.shape = (len(values), 1)
-        print(self.shape)
+            return (1, len(values[0]))    
+        return (len(values), 1)
+
+    def __init__(self, values):
+        self.values = values
+        self.shape = calculate_shape(values)
+    def __add__(self, x):
+        pass
+        
